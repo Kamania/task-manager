0aa5114ad9e2240580c5e16abc364f0803cb4fc6
Kamania <kamaniajoseph@gmail.com>
2023-08-10 15:36:03 +0300

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Http\Resources\TaskCollection;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    function index(Request $request)
+    {
+        return new TaskCollection(Task::all());
+    }
+
+    function show(Request $request, Task $task)
+    {
+        return new TaskResource($task);
+    }
+
+    function store(StoreTaskRequest $request)
+    {
+        $validated = $request->validated();
+
+        $task = Task::create($validated);
+
+        return new TaskResource($task);
+    }
+
+    function update(UpdateTaskRequest $request, Task $task){
+        $validated = $request->validated();
+
+        $task->update($validated);
+        return new TaskResource($task);
+    }
+
+    function destroy(Request $request, Task $task)
+    {
+        $task->delete();
+
+        return response()->noContent();
+    }
+}
